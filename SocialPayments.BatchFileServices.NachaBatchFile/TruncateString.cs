5cedfdcdbec16facf9700752789ac9f5b0647c00
James Rhodes <jrhodes2705@gmail.com>
2012-05-31 10:49:23 -0400

    added back Create Nacha File Job service

---
+++ SocialPayments.BatchFileServices.NachaBatchFile/TruncateString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialPayments.BatchFileServices.NachaBatchFile
+{
+    public static class TruncateString
+    {
+        public static string Truncate(this string str, int maxLength)
+        {
+            return str.Substring(0, Math.Min(str.Length, maxLength));
+        }
+    }
+}
