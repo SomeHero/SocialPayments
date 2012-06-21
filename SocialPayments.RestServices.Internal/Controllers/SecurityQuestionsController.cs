cd73a31c721dc9f2dec6972f26ab74d08eeb7014
Chris <chris@pdthx.me>
2012-06-21 14:48:48 -0400

    Added Security Questions Implementation

---
+++ SocialPayments.RestServices.Internal/Controllers/SecurityQuestionsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using SocialPayments.DataLayer;
+using NLog;
+using System.Net;
+using SocialPayments.Domain;
+using SocialPayments.RestServices.Internal.Models;
+
+
+namespace SocialPayments.RestServices.Internal.Controllers
+{
+    public class SecurityQuestionsController : ApiController
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        // GET /api/securityquestion
+        public HttpResponseMessage<List<SecurityQuestionModel.QuestionResponse>> Get()
+        {
+            Context _ctx = new Context();
+
+            List<SecurityQuestionModel.QuestionResponse> response = new List<SecurityQuestionModel.QuestionResponse>();
+
+            foreach (SecurityQuestion question in _ctx.SecurityQuestions)
+            {
+                response.Add(new SecurityQuestionModel.QuestionResponse()
+                {
+                    Id = question.Id,
+                    Question = question.Question,
+                    IsActive = question.IsActive
+                });
+            }
+
+            return new HttpResponseMessage<List<SecurityQuestionModel.QuestionResponse>>(response, HttpStatusCode.OK);
+        }
+
+        // GET /api/securityquestion/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST /api/securityquestion
+        public void Post(string value)
+        {
+        }
+
+        // PUT /api/securityquestion/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/securityquestion/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SocialPayments.RestServices.Internal/Models/SecurityQuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialPayments.RestServices.Internal.Models
+{
+    public class SecurityQuestionModel
+    {
+        public class QuestionResponse
+        {
+            public int Id { get; set; }
+            public string Question { get; set; }
+            public bool IsActive { get; set; }
+        }
+    }
+}
