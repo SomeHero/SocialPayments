382bda8614b2bdd0546a3c4149a1e89a4bb1e653
James Rhodes <jrhodes2705@gmail.com>
2012-07-03 13:37:22 -0400

    added mobile web site to solution

--- SocialPayments.RestServices.Internal/Controllers/UserPayStreamMessagesController.cs
+++ SocialPayments.RestServices.Internal/Controllers/UserPayStreamMessagesController.cs
@@ -19,119 +19,44 @@ namespace SocialPayments.RestServices.Internal.Controllers
         // GET /api/{userId}/PayStreamMessages
         public HttpResponseMessage<List<MessageModels.MessageResponse>> Get(string userId)
         {
-            Context _ctx = new Context();
-            DomainServices.MessageServices messageServices = new DomainServices.MessageServices(_ctx);
+           using(var _ctx = new Context())
+           {
+                DomainServices.MessageServices messageServices = new DomainServices.MessageServices(_ctx);
+                DomainServices.UserService userServices = new DomainServices.UserService(_ctx);
+                
+                var user = userServices.GetUserById(userId);
 
-            var user = GetUser(userId);
-
-            if (user == null)
-            {
-                var message = new HttpResponseMessage<List<MessageModels.MessageResponse>>(HttpStatusCode.NotFound);
-                message.ReasonPhrase = String.Format("User {0} specified in the request not found.", userId);
-
-                return message;
-            }
-
-            var messages = _ctx.Messages
-                .Where(m => m.SenderId == user.UserId || m.RecipientId.Value == user.UserId)
-                .OrderByDescending(m => m.CreateDate)
-                .ToList<Message>();
-
-            List<MessageModels.MessageResponse> messageResponse = new List<MessageModels.MessageResponse>();
-
-            URIType senderUriType = URIType.MECode;
-            URIType recipientUriType = URIType.MECode;
-
-            string senderName = "";
-            string recipientName = "";
-
-            try
-            {
-                foreach (var message in messages)
+                if (user == null)
                 {
-                    senderUriType = URIType.MECode;
-                    recipientUriType = URIType.MECode;
-                    
-                    senderUriType = messageServices.GetURIType(message.SenderUri);
-                    recipientUriType = messageServices.GetURIType(message.RecipientUri);
+                    var message = new HttpResponseMessage<List<MessageModels.MessageResponse>>(HttpStatusCode.NotFound);
+                    message.ReasonPhrase = String.Format("User {0} specified in the request not found.", userId);
 
-                    if (!String.IsNullOrEmpty(message.Sender.FirstName) || !String.IsNullOrEmpty(message.Sender.LastName))
-                        senderName = message.Sender.FirstName + " " + message.Sender.LastName;
-                    else if (!String.IsNullOrEmpty(message.senderFirstName) || !String.IsNullOrEmpty(message.senderLastName))
-                        senderName = message.senderFirstName + " " + message.Sender.LastName;
-                    else
-                        senderName = (senderUriType == URIType.MobileNumber ? _formattingServices.FormatMobileNumber(message.SenderUri) : message.SenderUri);
-
-                    if (message.Recipient != null && (!String.IsNullOrEmpty(message.Recipient.FirstName) || !String.IsNullOrEmpty(message.Recipient.LastName)))
-                        recipientName = message.Recipient.FirstName + " " + message.Recipient.LastName;
-                    else if (!String.IsNullOrEmpty(message.recipientFirstName) || !String.IsNullOrEmpty(message.recipientLastName))
-                        recipientName = message.recipientFirstName + " " + message.recipientLastName;
-                    else
-                        recipientName = (recipientUriType == URIType.MobileNumber ? _formattingServices.FormatMobileNumber(message.RecipientUri) : message.RecipientUri);
-
-                    _logger.Log(LogLevel.Error, String.Format("URI Formats {0} {1}", senderUriType, recipientUriType));
-
-                    String direction = "In";
-                    
-                    if (message.SenderId.Equals(user.UserId))
-                        direction = "Out";
-
-                    string transactionImageUrl = String.Empty;
-
-                    if (direction == "In")
-                    {
-                        if(message.Sender != null && !String.IsNullOrEmpty(message.Sender.ImageUrl))
-                            transactionImageUrl = message.Sender.ImageUrl;
-                    }
-                    else
-                    {
-                        if (message.Recipient != null && !String.IsNullOrEmpty(message.Recipient.ImageUrl))
-                            transactionImageUrl = message.Recipient.ImageUrl;
-                        else
-                            transactionImageUrl = message.recipientImageUri;
-                    }
-
-                    var status = message.Status;
-
-                    _logger.Log(LogLevel.Info, String.Format("Message Status {0}", status));
-                    
-                    messageResponse.Add(new MessageModels.MessageResponse()
-                    {
-                        amount = message.Amount,
-                        comments = (!String.IsNullOrEmpty(message.Comments) ? String.Format("{0}", message.Comments) : "No comments"),
-                        createDate = message.CreateDate.ToString("ddd MMM dd HH:mm:ss zzz yyyy"),
-                        Id = message.Id,
-                        //lastUpdatedDate =  m.LastUpdatedDate.ToString("ddd MMM dd HH:mm:ss zzz yyyy"),
-                        messageStatus =  message.Status.ToString(),
-                        messageType = message.MessageType.ToString(),
-                        recipientUri = (recipientUriType == URIType.MobileNumber ? _formattingServices.FormatMobileNumber(message.RecipientUri) : message.RecipientUri),
-                        senderUri = (senderUriType == URIType.MobileNumber ? _formattingServices.FormatMobileNumber(message.SenderUri) : message.SenderUri),
-                        direction = direction,
-                        latitude = message.Latitude,
-                        longitutde = message.Longitude,
-                        senderName = senderName,
-                        transactionImageUri = transactionImageUrl,
-                        recipientName = recipientName
-                    });
-
-                    _logger.Log(LogLevel.Error, String.Format("Added MEssage {0} {1}", senderUriType, recipientUriType));
-
+                    return message;
                 }
-            }
-            catch (Exception ex)
-            {
-                _logger.Log(LogLevel.Error, String.Format("Unhandled Exception formatting response. {0}", ex.Message));
-            }
 
-            if (messageResponse == null)
-            {
-                var message = new HttpResponseMessage<List<MessageModels.MessageResponse>>(HttpStatusCode.InternalServerError);
-                message.ReasonPhrase = String.Format("Unhandled exception get message for {0}", userId);
+                var messages = messageServices.GetMessages(user.UserId);
+                
+                var messageResponse = messages.Select(m => new MessageModels.MessageResponse() 
+                {
+                        amount = m.Amount,
+                        comments = (!String.IsNullOrEmpty(m.Comments) ? String.Format("{0}", m.Comments) : "No comments"),
+                        createDate = m.CreateDate.ToString("ddd MMM dd HH:mm:ss zzz yyyy"),
+                        Id = m.Id,
+                        //lastUpdatedDate =  m.LastUpdatedDate.ToString("ddd MMM dd HH:mm:ss zzz yyyy"),
+                        messageStatus =  m.Status.ToString(),
+                        messageType = m.MessageType.ToString(),
+                        recipientUri = m.RecipientUri,
+                        senderUri = m.SenderUri,
+                        direction = m.Direction,
+                        latitude = m.Latitude,
+                        longitutde = m.Longitude,
+                        senderName = m.SenderName,
+                        transactionImageUri = m.TransactionImageUrl,
+                        recipientName = m.RecipientName
+                    }).ToList();
 
-                return message;
+                return new HttpResponseMessage<List<MessageModels.MessageResponse>>(messageResponse, HttpStatusCode.OK);
             }
-
-            return new HttpResponseMessage<List<MessageModels.MessageResponse>>(messageResponse, HttpStatusCode.OK);
         }
 
         private User GetUser(string id)
