474624cd2cf4cb0ba1437658ca6490562c54bebf
James Rhodes <jrhodes2705@gmail.com>
2012-07-03 13:59:40 -0400

    fixed adding Mobile PaidThx

---
+++ Mobile PaidThx/Scripts/register.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="jquery-1.5.1-vsdoc.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+
+$(document).ready(function () {
+    $("#frmRegister").validate({
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
+
+var registerUser = function (registrationData, onSuccess) {
+
+    var serviceUrl = 'http://beta.paidthx.me/services/UserService/Register?apiKey=BDA11D91-7ADE-4DA1-855D-24ADFE39D174';
+    var jsonData = $.toJSON(registrationData);
+
+    $.ajax({
+        type: "POST",
+        url: "http://beta.paidthx.me/services/UserService/Register?apiKey=BDA11D91-7ADE-4DA1-855D-24ADFE39D174",
+        data: jsonData,
+        contentType: "application/json",
+        dataType: "json",
+        processData: false,
+        success: function (data) {
+            onSuccess(data);
+        },
+        error: function (objRequest, next, errorThrown) {
+            alert(objRequest.responseText);
+        }
+    });
+}
