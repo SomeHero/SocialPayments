f873c38946eb7fd3a6ad987dec4ccbdd09954600
unknown <Justin@Justin-PC.(none)>
2012-07-12 12:12:09 -0400

    Justin's Editing and Add ACH Functions

--- Mobile PaidThx/Controllers/PaymentAccountController.cs
+++ Mobile PaidThx/Controllers/PaymentAccountController.cs
@@ -36,15 +36,18 @@ namespace Mobile_PaidThx.Controllers
                 var model = new ListPaymentAccountModel();
                 foreach (var paymentAccount in user.PaymentAccounts)
                 {
-                    model.PaymentAccounts.Add(new BankAccountModel()
+                    if (paymentAccount.IsActive == true)
                     {
-                        PaymentAccountId = paymentAccount.Id.ToString(),
-                        AccountNumber = securityService.Decrypt(paymentAccount.AccountNumber),
-                        AccountType = paymentAccount.AccountType.ToString(),
-                        NameOnAccouont = securityService.Decrypt(paymentAccount.NameOnAccount),
-                        Nickname = "",
-                        RoutingNumber = securityService.Decrypt(paymentAccount.RoutingNumber)
-                    });
+                        model.PaymentAccounts.Add(new BankAccountModel()
+                        {
+                            PaymentAccountId = paymentAccount.Id.ToString(),
+                            AccountNumber = securityService.Decrypt(paymentAccount.AccountNumber),
+                            AccountType = paymentAccount.AccountType.ToString(),
+                            NameOnAccouont = securityService.Decrypt(paymentAccount.NameOnAccount),
+                            Nickname = "",
+                            RoutingNumber = securityService.Decrypt(paymentAccount.RoutingNumber)
+                        });
+                    }
                 }
 
                 return PartialView("PartialViews/List", model);
@@ -80,12 +83,18 @@ namespace Mobile_PaidThx.Controllers
 
                     var paymentAccount = new SocialPayments.Domain.PaymentAccount()
                     {
+                        Nickname = model.Nickname,
                         AccountNumber = securityService.Encrypt(model.AccountNumber),
                         PaymentAccountTypeId = paymentAccountTypeId,
                         NameOnAccount = securityService.Encrypt(model.NameOnAccount),
                         RoutingNumber = securityService.Encrypt(model.RoutingNumber),
                         UserId = userId,
-                        Id = Guid.NewGuid()
+                        Id = Guid.NewGuid(),
+                        CreateDate = System.DateTime.Now,
+                        AccountStatus = SocialPayments.Domain.AccountStatusType.Submitted,
+                        IsActive = true,
+                        BankIconURL = "http://images.PaidThx.com/BankIcons/bank.png",
+                        BankName = "Temp"
                     };
 
                     ctx.PaymentAccounts.Add(paymentAccount);
@@ -107,6 +116,7 @@ namespace Mobile_PaidThx.Controllers
                 }
             }
         }
+   
         public PartialViewResult Edit(string Id)
         {
             // if (Session["UserId"] == null)
@@ -115,49 +125,85 @@ namespace Mobile_PaidThx.Controllers
             {
                 var userId = (Guid)Session["UserId"];
                 var user = ctx.Users.FirstOrDefault(u => u.UserId == userId);
-
                 //if (Session["User"] == null)
                 // return RedirectToAction("SignIn", "Account", null);
+                
 
-                var paymentAccount = user.PaymentAccounts.FirstOrDefault(a => a.Id == new Guid(Id));
-                var securityService = new SocialPayments.DomainServices.SecurityService();
+                    var paymentAccount = user.PaymentAccounts.FirstOrDefault(a => a.Id == new Guid(Id));
+                    var securityService = new SocialPayments.DomainServices.SecurityService();
 
-                return PartialView("PartialViews/Edit", new EditPaymentAccountModel()
-                {
-                    AccountNumber = securityService.Decrypt(paymentAccount.AccountNumber),
-                    AccountType = paymentAccount.AccountType.ToString(),
-                    NameOnAccount = securityService.Decrypt(paymentAccount.NameOnAccount),
-                    Nickname = "",
-                    RoutingNumber = securityService.Decrypt(paymentAccount.RoutingNumber),
-                    PaymentAccountId = paymentAccount.Id.ToString()
-                });
-
+                    return PartialView("PartialViews/Edit", new EditPaymentAccountModel()
+                    {
+                        AccountNumber = securityService.Decrypt(paymentAccount.AccountNumber),
+                        AccountType = paymentAccount.AccountType.ToString(),
+                        NameOnAccount = securityService.Decrypt(paymentAccount.NameOnAccount),
+                        Nickname = paymentAccount.Nickname,
+                        RoutingNumber = securityService.Decrypt(paymentAccount.RoutingNumber),
+                        PaymentAccountId = paymentAccount.Id.ToString()
+                    });
             }
         }
         [HttpPost]
-        public ActionResult Edit(EditPaymentAccountModel model)
+        public ActionResult Edit(EditPaymentAccountModel model, string Id)
         {
             if (Session["UserId"] == null)
                 return RedirectToAction("SignIn", "Account", null);
             using (var ctx = new Context())
-            {
+            {               
                 var userId = (Guid)Session["UserId"];
                 var user = ctx.Users.FirstOrDefault(u => u.UserId == userId);
+                var securityService = new SocialPayments.DomainServices.SecurityService();
+                var paymentAccount = user.PaymentAccounts.FirstOrDefault(a => a.Id == new Guid(Id));
+                var paymentAccountTypeId = (int)SocialPayments.Domain.PaymentAccountType.Savings;
 
-                if (Session["User"] == null)
-                    return RedirectToAction("SignIn", "Account", null);
-
-                var paymentAccount = user.PaymentAccounts.Where(a => a.Id == new Guid(model.PaymentAccountId));
-
+                if (model.AccountType.ToLower().Equals("checking"))
+                    paymentAccountTypeId = (int)SocialPayments.Domain.PaymentAccountType.Checking;
                 if (paymentAccount == null)
                 {
                     ModelState.AddModelError("", "Unable to edit payment account");
 
                     return View(model);
                 }
-
-                return View(model);
+                
+                paymentAccount.AccountNumber = securityService.Encrypt(model.AccountNumber);
+                paymentAccount.NameOnAccount = securityService.Encrypt(model.NameOnAccount);
+                paymentAccount.Nickname = model.Nickname;
+                paymentAccount.RoutingNumber = securityService.Encrypt(model.RoutingNumber);
+                paymentAccount.PaymentAccountTypeId = paymentAccountTypeId;
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    logger.Log(LogLevel.Error, String.Format("Unhandled Exception Editing Payment Account. {0}", ex.Message));
+                }
+               
+                return RedirectToAction("List");
+                
             }
+
+        }
+        [HttpPost]
+        public ActionResult Remove(string Id, EditPaymentAccountModel model)
+        {
+            using (var ctx = new Context())
+            {
+                var userId = (Guid)Session["UserId"];
+                var user = ctx.Users.FirstOrDefault(u => u.UserId == userId);
+                var paymentAccount = user.PaymentAccounts.FirstOrDefault(a => a.Id == new Guid(Id));
+                paymentAccount.IsActive = false;
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    logger.Log(LogLevel.Error, String.Format("Unhandled Exception Deleting account. {0}", ex.Message));
+                }
+                return RedirectToAction("List");
+            }
+
         }
     }
     
