72016f37a073794feaf4a67f7f380d91165e9ebc
Ivy Jeong <ivy@pdthx.me>
2012-08-14 17:24:47 -0400

    added email, phone, and mecode screens to preferences

--- Mobile PaidThx/Services/ServicesBase.cs
+++ Mobile PaidThx/Services/ServicesBase.cs
@@ -57,11 +57,113 @@ namespace Mobile_PaidThx.Services
                 JsonResponse = jsonResponse
             };
         }
+
+        protected ServiceResponse Delete(string serviceUrl)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(serviceUrl);
+            req.Method = "DELETE";
+
+            HttpStatusCode statusCode = HttpStatusCode.OK;
+
+            string statusReason = String.Empty;
+            string jsonResponse = String.Empty;
+
+            try
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)req.GetResponse();
+
+                statusCode = httpResponse.StatusCode;
+                statusReason = httpResponse.StatusDescription;
+
+                using (StreamReader sr = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    jsonResponse = sr.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                using (WebResponse response = ex.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+
+                    statusCode = httpResponse.StatusCode;
+                    statusReason = httpResponse.StatusDescription;
+
+                }
+            }
+
+            if (statusCode.Equals(HttpStatusCode.BadRequest))
+            {
+                throw new Exception(statusReason);
+            }
+
+            return new ServiceResponse()
+            {
+                StatusCode = statusCode,
+                Description = statusReason,
+                JsonResponse = jsonResponse
+            };
+        }
+
         protected ServiceResponse Post(string serviceUrl, string json)
         {
             // Create new HTTP request.
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(serviceUrl);
             req.Method = "POST";
+            req.ContentType = "application/json";
+            byte[] postData = Encoding.ASCII.GetBytes(json);
+            req.ContentLength = postData.Length;
+
+            // Send HTTP request.
+            Stream PostStream = req.GetRequestStream();
+            PostStream.Write(postData, 0, postData.Length);
+            HttpStatusCode statusCode = HttpStatusCode.OK;
+
+            string statusReason = String.Empty;
+            string jsonResponse = String.Empty;
+
+            try
+            {
+                HttpWebResponse httpResponse = (HttpWebResponse)req.GetResponse();
+
+                statusCode = httpResponse.StatusCode;
+                statusReason = httpResponse.StatusDescription;
+
+                using (StreamReader sr = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    jsonResponse = sr.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                using (WebResponse response = ex.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+
+                    statusCode = httpResponse.StatusCode;
+                    statusReason = httpResponse.StatusDescription;
+
+                }
+            }
+
+            if (statusCode.Equals(HttpStatusCode.BadRequest))
+            {
+                throw new Exception(statusReason);
+            }
+
+            return new ServiceResponse()
+            {
+                StatusCode = statusCode,
+                Description = statusReason,
+                JsonResponse = jsonResponse
+            };
+        }
+
+        protected ServiceResponse Put(string serviceUrl, string json)
+        {
+            // Create new HTTP request.
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(serviceUrl);
+            req.Method = "PUT";
             req.ContentType = "application/json";
             byte[] postData = Encoding.ASCII.GetBytes(json);
             req.ContentLength = postData.Length;
